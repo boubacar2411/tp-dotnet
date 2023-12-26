3140034758284d73867916e22534d7ead885a80a
unknown <boubacar032@gmail.com>
2023-12-26 21:55:12 +0000

    Votre message de commit

---
+++ Projet-mobile/phoneword/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace phoneword;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
